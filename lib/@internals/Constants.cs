116800eba072f2c852a5b2e5b94c0946109475e9
Yuuki Wesp <ls-micro@ya.ru>
2020-03-07 11:31:39 +0300

    add contstants

---
+++ lib/@internals/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Elementary.@internal
+{
+    internal static class Constants
+    {
+        public const double ElectronMassPlank = 1.6000000000000002E-19;
+        /// <summary>
+        /// c^2
+        /// </summary>
+        public const double SpeedOfLightSquared = 9E+16;
+    }
+}
