9aa1d46442833c953095cb94205536fbdc9df179
Yuuki Wesp <ls-micro@ya.ru>
2020-03-08 14:33:15 +0300

    add interaction flag

---
+++ lib/Primitives/InteractionFlags.cs
@@ -0,0 +1,18 @@
+﻿namespace Elementary.Primitives
+{
+    using System;
+
+    [Flags]
+    public enum InteractionFlags : byte
+    {
+        None            = 0x0,
+        Electromagnetic = 0x1 << 0,
+        Weak            = 0x1 << 1,
+        Strong          = 0x1 << 2,
+        Gravity         = 0x1 << 3,
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Fifth_force
+        /// </summary>
+        Quintessence    = 0x1 << 4
+    }
+}
